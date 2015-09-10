a03c32f06be91825091eceb6e2fd04b8bcf800d0
Randall Degges <r@rdegges.com>
2015-09-10 16:00:28 -0700

    Adding some tests for our Config object / constructor.

---
+++ test/test_config.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var assert = require('assert');
+
+var Config = require('../index');
+
+describe('Config', function() {
+  describe('#()', function() {
+    it('should accept no input', function() {
+      assert.doesNotThrow(function() {
+        var config = new Config();
+      }, Error);
+    });
+
+    it('should accept json input', function() {
+      assert.doesNotThrow(function() {
+        var config = new Config({ hi: 'there' });
+      }, Error);
+    });
+
+    it('should bind all passed values to itself', function() {
+      var config = new Config({ hi: 'there', yo: 'momma', test: { nested: 'stuff' } });
+      assert.equal(config.hi, 'there');
+      assert.equal(config.hi, 'momma');
+      assert.equal(config.test, { nested: 'stuff' });
+    });
+
+    it('should bind additional properties to itself', function() {
+      var config = new Config({ hi: 'there' });
+      config.yo = 'momma';
+      assert.equal(config.hi, 'there');
+      assert.equal(config.yo, 'momma');
+    });
+  });
+});
